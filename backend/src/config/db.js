9a1c947ab862c7b9df74b648e7fd9f4c8b3d960d
yuv <2206yatharth@gmail.com>
2026-05-03 17:27:48 +0530

    fix: add ssl for supabase

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,6 +5,9 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const query = (text, params) => pool.query(text, params);
